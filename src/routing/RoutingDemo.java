98293aa5d1b772df2c965d0a94ad8247c0672c2d
Frédéric Jamar <isanzo@mbp-de-frederic.umons.ac.be>
2017-12-21 19:24:21 +0100

    Adding init lines

--- src/routing/RoutingDemo.java
+++ src/routing/RoutingDemo.java
@@ -1,8 +1,24 @@
 package routing;
 
+import reso.scheduler.AbstractScheduler;
+import reso.scheduler.Scheduler;
+
 public class RoutingDemo {
 
     public static final String TOPO_FILE= "reso/data/topology.txt";
 
-    
+    public static void main(String [] args) {
+
+        String filename= RoutingDemo.class.getClassLoader().getResource(TOPO_FILE).getFile();
+
+        AbstractScheduler scheduler= new Scheduler();
+
+        try{
+
+        }
+        catch (Exception e) {
+        System.err.println(e.getMessage());
+        e.printStackTrace(System.err);
+    }
+    }
 }
