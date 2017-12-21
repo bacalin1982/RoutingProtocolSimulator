33f5f82986ae50aa4b8322c8d7b97dc999a1f1ae
Frédéric Jamar <isanzo@mbp-de-frederic.umons.ac.be>
2017-12-21 20:17:17 +0100

    Termination of Demo Class.

--- src/routing/RoutingDemo.java
+++ src/routing/RoutingDemo.java
@@ -1,8 +1,11 @@
 package routing;
 
 import reso.common.Network;
+import reso.common.Node;
+import reso.ip.IPRouter;
 import reso.scheduler.AbstractScheduler;
 import reso.scheduler.Scheduler;
+import reso.utilities.FIBDumper;
 import reso.utilities.NetworkBuilder;
 
 public class RoutingDemo {
@@ -17,6 +20,22 @@ public class RoutingDemo {
 
         try{
             Network network= NetworkBuilder.loadTopology(filename, scheduler);
+
+            // Add routing protocol application to each router
+            for (Node n: network.getNodes()) {
+                if (!(n instanceof IPRouter))
+                    continue;
+                IPRouter router= (IPRouter) n;
+                boolean advertise= true;
+                router.addApplication(new LSPRoutingProtocol(router, advertise));
+                router.start();
+            }
+
+            // Run simulation
+            scheduler.run();
+
+            // Display forwarding table for each node
+            FIBDumper.dumpForAllRouters(network);
         }
         catch (Exception e) {
             System.err.println(e.getMessage());
