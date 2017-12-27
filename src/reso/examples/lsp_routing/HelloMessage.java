3fbd96e6cc48c8a565746a1711978b43704d3948
Frédéric Jamar <iSanzo@Mac-Pro-de-Frederic.local>
2017-12-27 10:08:01 +0100

    First implementation of HelloMessage Class

--- src/reso/examples/lsp_routing/HelloMessage.java
+++ src/reso/examples/lsp_routing/HelloMessage.java
@@ -1,4 +1,43 @@
 package reso.examples.lsp_routing;
 
-public class HelloMessage {
+import reso.common.Message;
+import reso.ip.IPAddress;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloMessage implements Message {
+    public static class Hello {
+        public final IPAddress origin;
+        public List<IPAddress> neighbours;
+
+        public Hello(IPAddress origin, List<IPAddress> neighbours) {
+            this.origin = origin;
+            this.neighbours = neighbours;
+        }
+
+        public String toString() {
+            String ns = "";
+            for (IPAddress n : neighbours)
+                ns += n.toString() + "; ";
+            return "Hello[" + origin + "," + ns + "]";
+        }
+    }
+
+    public final List<Hello> Hellos;
+
+    public HelloMessage() {
+        Hellos = new ArrayList<Hello>();
+    }
+
+    public void addHello(IPAddress origin, List<IPAddress> neighbours) {
+        Hellos.add(new Hello(origin, neighbours));
+    }
+
+    public String toString() {
+        String s = "";
+        for (Hello m : Hellos)
+            s += " " + m.toString();
+        return s;
+    }
 }
