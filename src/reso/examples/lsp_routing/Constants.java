419609f991f9d1baa3fcb427e62e6a0aa8c29f25
Frédéric Jamar <iSanzo@Mac-Pro-de-Frederic.local>
2017-12-27 11:31:52 +0100

    Adding constants string class for output messages

---
+++ src/reso/examples/lsp_routing/Constants.java
@@ -0,0 +1,15 @@
+package reso.examples.lsp_routing;
+
+public class Constants {
+    /* Title */
+    public static final String _T = " ---------------------- "+
+                                    "| LSP Routing Protocol |"+
+                                    "|     B.C. &  F.J.     |"+
+                                    " ---------------------- ";
+    /* Headers */
+    public static final String _I = "INFO   : ";
+    public static final String _E = "ERROR  : ";
+    public static final String _W = "WARNING: ";
+
+    /* Strings */
+}
